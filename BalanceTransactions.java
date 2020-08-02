dda43670efad4ef983217b9da665ff90d235277e
Megan Jane Thompson <59883929+megjthom@users.noreply.github.com>
2020-08-02 13:24:25 -0500

    Update BalanceTransactions.java

--- BalanceTransactions.java
+++ BalanceTransactions.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 
 /** 
  * Title: Assignment 5 
- * Semester: COP3337 – Fall 2019
+ * Semester: COP3337 â€“ Fall 2019
  * @author Megan Jane Thompson
- *
- * I affirm that this program is entirely my own work
- * and none of it is the work of any other person.
  * 
  * This program calculates daily transactions in a text file. 
  * It calculates if the actual amount of cash at the end of 
